8418f58cfb3c22a672caf764c0da1c9102a6354d
Shawn Rice <rice.shawn@gmail.com>
2014-04-21 01:43:26 -0400

    initial

---
+++ README.txt
@@ -0,0 +1,65 @@
+=== @TODO: Reckoning ===
+Contributors: shawn-patrick-rice
+Donate link: https://www.paypal.com/cgi-bin/webscr?cmd=_donations&business=rice@shawnrice.com
+Tags: comments, posts, tools, reckoning, reckon, tally, assessment
+Requires at least: 3.5.1
+Tested up to: 3.9
+Stable tag: 1.0.0
+License: GPLv2 or later
+License URI: http://www.gnu.org/licenses/gpl-2.0.html
+
+Adds a submenu (under tools) that tallies all the users' posts and comments on a blog, especially useful for assement of blogs for classes.
+
+== Description ==
+
+
+== Installation ==
+
+Just install like you would regularly. This plugin does not create any table or data; it simply displays some data nicely.
+
+= Using The WordPress Dashboard =
+
+1. Navigate to the 'Add New' in the plugins dashboard
+2. Search for 'reckoning'
+3. Click 'Install Now'
+4. Activate the plugin on the Plugin dashboard
+
+= Uploading in WordPress Dashboard =
+
+1. Navigate to the 'Add New' in the plugins dashboard
+2. Navigate to the 'Upload' area
+3. Select `reckoning.zip` from your computer
+4. Click 'Install Now'
+5. Activate the plugin in the Plugin dashboard
+
+= Using FTP =
+
+1. Download `reckoning.zip`
+2. Extract the `reckoning` directory to your computer
+3. Upload the `reckoning` directory to the `/wp-content/plugins/` directory
+4. Activate the plugin in the Plugin dashboard
+
+
+== Changelog ==
+
+= 1.0 =
+* A change since the previous version.
+* Another change.
+
+= 0.5 =
+* List versions from most recent at top to oldest at bottom.
+
+== Upgrade Notice ==
+
+= 1.0 =
+Upgrade notices describe the reason a user should upgrade.  No more than 300 characters.
+
+= 0.5 =
+This version fixes a security related bug.  Upgrade immediately.
+
+== Arbitrary section ==
+
+You may provide arbitrary sections, in the same format as the ones above.  This may be of use for extremely complicated
+plugins where more information needs to be conveyed that doesn't fit into the categories of "description" or
+"installation."  Arbitrary sections will be shown below the built-in sections outlined above.
+
---
+++ reckoning.php
@@ -0,0 +1,82 @@
+<?php
+/**
+ * The Reckoning
+ *
+ * Adds a submenu (under tools) that tallies all the users' posts and comments on a blog. This
+ * is especially useful for assement of blogs for classes.
+ *
+ * @package   Reckoning
+ * @author    Shawn Patrick Rice <rice@shawnrice.org>
+ * @license   GPL-2.0+
+ * @link      http://example.com
+ * @copyright 2014 Shawn Patrick Rice
+ *
+ * @wordpress-plugin
+ * Plugin Name:       Reckoning
+ * Plugin URI:        @TODO
+ * Description:       @TODO
+ * Version:           1.0.0
+ * Author:            Shawn Patrick Rice
+ * Author URI:        shawnrice.org
+ * Text Domain:       reckoning-locale
+ * License:           GPL-2.0+
+ * License URI:       http://www.gnu.org/licenses/gpl-2.0.txt
+ * Domain Path:       /languages
+ * GitHub Plugin URI: https://github.com/<owner>/<repo>
+ */
+
+if ( ! defined( 'WPINC' ) ) {
+	die;
+}
+
+if ( is_admin() && ( ! defined( 'DOING_AJAX' ) || ! DOING_AJAX ) ) {
+	add_action('admin_menu', 'reckoning_admin_menu');
+	add_action( 'admin_enqueue_scripts', 'enqueue_reckoning_admin_styles' );
+}
+
+function reckoning_admin_menu() {
+  add_submenu_page('tools.php', 'Reckoning', 'Reckoning', 'manage_options', 'Reckoning', 'display_reckoning_admin_page');
+}
+
+function enqueue_reckoning_admin_styles() {
+	wp_enqueue_style( 'reckoning-admin-styles', plugins_url( 'admin.css', __FILE__ ));
+}
+
+function display_reckoning_admin_page() {
+	?>
+	<div class="wrap">
+	<h2><?php echo esc_html( get_admin_page_title() ); ?></h2>
+
+<?php
+	global $wpdb;
+	$date_format='m/d/Y';
+	$blog = get_blog_details();
+	$users = get_users();
+	echo '<h2 class="entry-title">Summary of Activity on "' . $blog->blogname .'"</h2>';
+	foreach ($users as $user) {
+		echo "<table class = 'reckoning-table'>";
+		$user_id=$user->data->ID;
+		$where = 'WHERE comment_approved = 1 AND user_id = ' . $user_id ;
+		$comment_count = $wpdb->get_var(
+		"SELECT COUNT( * ) AS total
+		FROM {$wpdb->comments}
+		{$where}
+		");
+		$post_count = get_usernumposts($user_id);
+		$posts = get_posts( array('author' => $user_id) );
+
+		echo '<h3>' . $user->data->user_nicename . '</h3>';
+		foreach ( $posts as $post ) {
+			echo '<tr>';
+			$date = date( $date_format , mktime($post->post_date));
+			echo "<td><a href='$post->guid'>$post->post_title</a></td><td>$date</td>";
+			echo '</tr>';
+		}
+		echo '<tr><td>Total Posts</td><td>' . $post_count .'</td></tr>';
+		echo '<tr><td>Total Comments</td><td>' . $comment_count . '</td></tr>';
+		echo '</table>';
+	}
+?>
+	</div>
+<?php
+}
