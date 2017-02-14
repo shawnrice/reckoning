c7d7aec902ae2882b703b46bfa6bac6ede38036d
Shawn Patrick Rice <rice@shawnrice.org>
2017-02-14 03:28:34 -0500

    finish changing license

--- reckoning.php
+++ reckoning.php
@@ -7,37 +7,21 @@
  *
  * @package   Reckoning
  * @author    Shawn Patrick Rice <rice@shawnrice.org>
- * @license   GPL-2.0+
- * @link      http://example.com
+ * @license   MIT
+ * @link      https://github.com/shawnrice/wp-reckoning
  * @copyright 2014 Shawn Patrick Rice
  *
  * @wordpress-plugin
  * Plugin Name:       Reckoning
- * Plugin URI:        http://shawnrice.github.io/wp-reckoning/
+ * Plugin URI:        https://github.com/shawnrice/wp-reckoning
  * Description:       Tallies posts / comments per user per blog.
  * Version:           2.0.0
  * Author:            Shawn Patrick Rice
  * Author URI:        http://shawnrice.org
  * Text Domain:		   reckoning
- * License:           GPL-2.0+
- * License URI:       http://www.gnu.org/licenses/gpl-2.0.txt
+ * License:           MIT
+ * License URI:       https://opensource.org/licenses/MIT
  * GitHub Plugin URI: https://github.com/shawnrice/wp-reckoning
- */
-
-/*  Copyright 2104  Shawn Patrick Rice  (email : rice@shawnrice.org)
- *
- *  This program is free software; you can redistribute it and/or modify
- *  it under the terms of the GNU General Public License, version 2, as
- *  published by the Free Software Foundation.
- *
- *  This program is distributed in the hope that it will be useful,
- *  but WITHOUT ANY WARRANTY; without even the implied warranty of
- *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- *  GNU General Public License for more details.
- *
- *  You should have received a copy of the GNU General Public License
- *  along with this program; if not, write to the Free Software
- *  Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
  */
 
 if ( ! defined( 'WPINC' ) ) {
